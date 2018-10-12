fd6a516d5d83bd27e19191883f531cd1ea5f475e
tkarrer <thibault.karrer@gmail.com>
2018-10-12 09:57:01 +0000

    New day 1

---
+++ tests/base.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class BaseJumpsToListTest(unittest.TestCase):
+    pass
---
+++ tests/test_sample.py
@@ -0,0 +1,13 @@
+import unittest
+
+from tests.base import BaseJumpsToListTest
+
+
+class SampleTest(BaseJumpsToListTest):
+
+    def test_sample(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
