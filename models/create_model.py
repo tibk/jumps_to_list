d6369c6d2ba8ae2628695033c79d2a196cb47d5a
tibk <thibault.karrer@gmail.com>
2019-04-26 08:22:43 +0000

    Lint

--- models/create_model.py
+++ models/create_model.py
@@ -4,15 +4,18 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
+
 Base = declarative_base()
- 
+
+
 class Jumper(Base):
     __tablename__ = 'jumper'
     # Here we define columns for the table jumper
     id = Column(Integer, primary_key=True)
     jumper_name = Column(String(250), nullable=False)
- 
+
+
 class Spot(Base):
     __tablename__ = 'spot'
     # Here we define columns for the table spot
@@ -20,7 +23,8 @@ class Spot(Base):
     spot_name = Column(String(250), nullable=False)
     spot_location = Column(String(250), nullable=False)
     spot_height = Column(Integer, nullable=False)
- 
+
+
 class Suit(Base):
     __tablename__ = 'suit'
     # Here we define columns for the table suit
@@ -28,13 +32,15 @@ class Suit(Base):
     suit_name = Column(String(250), nullable=False)
     suit_brand = Column(String(250), nullable=False)
     suit_kind = Column(String(250), nullable=False)
- 
+
+
 class JumpeKind(Base):
     __tablename__ = 'jump_kind'
     # Here we define columns for the table jump_kind
     id = Column(Integer, primary_key=True)
     jumper_kind = Column(String(250), nullable=False)
- 
+
+
 class Jump(Base):
     __tablename__ = 'jump'
     # Here we define columns for the table jump
@@ -49,7 +55,8 @@ class Jump(Base):
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
 engine = create_engine('postgresql://tkarrer@localhost/base_statistics')
- 
+
+
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
 Base.metadata.create_all(engine)
