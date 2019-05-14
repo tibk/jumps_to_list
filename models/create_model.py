d965d4f34a4cf9cd32df9d5528c9f43f59c6817f
tibk <thibault.karrer@gmail.com>
2019-05-14 16:14:10 +0000

    Rename cols

--- models/create_model.py
+++ models/create_model.py
@@ -13,32 +13,32 @@ class Jumper(Base):
     __tablename__ = 'jumper'
     # Here we define columns for the table jumper
     id = Column(Integer, primary_key=True)
-    jumper_name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
 
 
 class Spot(Base):
     __tablename__ = 'spot'
     # Here we define columns for the table spot
     id = Column(Integer, primary_key=True)
-    spot_name = Column(String(250), nullable=False)
-    spot_location = Column(String(250), nullable=False)
-    spot_height = Column(Integer, nullable=False)
+    name = Column(String(250), nullable=False, unique=True)
+    location = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
 
 
 class Suit(Base):
     __tablename__ = 'suit'
     # Here we define columns for the table suit
     id = Column(Integer, primary_key=True)
-    suit_name = Column(String(250), nullable=False)
-    suit_brand = Column(String(250), nullable=False)
-    suit_kind = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    brand = Column(String(250), nullable=False)
+    kind = Column(String(250), nullable=False)
 
 
-class JumpeKind(Base):
+class JumpKind(Base):
     __tablename__ = 'jump_kind'
     # Here we define columns for the table jump_kind
     id = Column(Integer, primary_key=True)
-    jumper_kind = Column(String(250), nullable=False)
+    kind = Column(String(250), nullable=False)
 
 
 class Jump(Base):
@@ -50,7 +50,7 @@ class Jump(Base):
     spot_id = Column(Integer, ForeignKey('spot.id'))
     suit_id = Column(Integer, ForeignKey('suit.id'))
     jump_kind_id = Column(Integer, ForeignKey('jump_kind.id'))
-    jump_comments = Column(String(500))
+    comments = Column(String(500))
 
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
